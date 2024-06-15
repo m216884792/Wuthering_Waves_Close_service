6e8866049aaae54e4f916817ccb652654269cb38
Don Nimis <m21688479@gmail.com>
2024-06-16 02:29:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if int(run(sc query "Wurthering_Wave_Close_service" | find /c "FAILED", shell=Tr
 
 while True:
   sleep(5)
-  if len(run(f'tasklist | find "Wuthering Waves.exe"', shell=True, capture_output=True, text=True))>0: #有無運行
+  if len(run(f'tasklist | find "Wuthering Waves.exe"', shell=True, capture_output=True, text=True))>10: #有無運行
     if int(run('sc query "AntiCheatExpert Service" | find /c "START"', shell=True, capture_output=True, text=True))>0:  #服務狀態
       run('sc stop "AntiCheatExpert Service"', shell=True)  #關閉服務
