c77909d23f56166dcfc67c273477f59eca11c7eb
Don Nimis <m21688479@gmail.com>
2024-06-15 23:32:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from subprocess import Popen,check_output
 
 
 if Popen(sc query "AntiCheatExpert Service" | find /c "FAILED")!=0:
-  sc create "Wurthering_Wave_Close_service" binpath = "{os.getcwd()}/main.exe" start= auto displayname = "鳴潮自動停止ACE服務"
-  sc start "Wurthering_Wave_Close_service"
+  Popen(f'sc create "Wurthering_Wave_Close_service" binpath = "{os.getcwd()}/main.exe" start= auto displayname = "鳴潮自動停止ACE服務"')
+  Popen('sc start "Wurthering_Wave_Close_service"')
 
 check_output(f'tasklist | find "Wuthering Waves.exe"', shell=True)
 if Popen('sc query "AntiCheatExpert Service" | find /c "START"')>0:
