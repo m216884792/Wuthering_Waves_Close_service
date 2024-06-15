6459fe79dd22a4a069a3653d1b22390e78f7b61a
Don Nimis <m21688479@gmail.com>
2024-06-16 00:12:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from subprocess import Popen,check_output
+from subprocess import Popen,check_output,run
 from time import sleep
 
 
-if Popen(sc query "AntiCheatExpert Service" | find /c "FAILED")!=0: #沒有服務
-  Popen(f'sc create "Wurthering_Wave_Close_service" binpath = "{os.getcwd()}/main.exe" start= auto displayname = "鳴潮自動停止ACE服務"') #創建服務
-  Popen('sc start "Wurthering_Wave_Close_service"') #啟動服務
+if int(run(sc query "AntiCheatExpert Service" | find /c "FAILED", shell=True, capture_output=True, text=True))!=0: #沒有服務
+  run(f'sc create "Wurthering_Wave_Close_service" binpath = "{os.getcwd()}/main.exe" start= auto displayname = "鳴潮自動停止ACE服務"', shell=True) #創建服務
+  run('sc start "Wurthering_Wave_Close_service"', shell=True) #啟動服務
 
 while True:
   sleep((5)
-  if len(check_output(f'tasklist | find "Wuthering Waves.exe"', shell=True))>0: #有無運行
-    if int(Popen('sc query "AntiCheatExpert Service" | find /c "START"'))>0:  #服務狀態
-      Popen('sc stop "AntiCheatExpert Service"')  #關閉服務
+  if len(run(f'tasklist | find "Wuthering Waves.exe"', shell=True, capture_output=True, text=True))>0: #有無運行
+    if int(run('sc query "AntiCheatExpert Service" | find /c "START"', shell=True, capture_output=True, text=True))>0:  #服務狀態
+      run('sc stop "AntiCheatExpert Service"', shell=True)  #關閉服務
