84d01ad6ce6c787830037f0346e81840b5ce4178
Don Nimis <m21688479@gmail.com>
2024-06-16 02:38:19 +0800

    Update groceries.py

--- mod/groceries.py
+++ mod/groceries.py
@@ -1,1 +1,8 @@
-xx
+import configparser
+
+
+def read_file_path_config(path) -> str | None:
+    """讀取路徑"""
+    config = configparser.ConfigParser()
+    config.read(f"{path}/config.ini", encoding='utf-8')
+    path_config = config.get("path", "log_path")xx
