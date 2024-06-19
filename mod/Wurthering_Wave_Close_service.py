39ece0ac8c8c0c0c81970f948c3222d9e8f16a74
Don Nimis <m21688479@gmail.com>
2024-06-19 16:38:27 +0800

    Update Wurthering_Wave_Close_service.py

--- mod/Wurthering_Wave_Close_service.py
+++ mod/Wurthering_Wave_Close_service.py
@@ -1,11 +1,32 @@
+
 from time import sleep
 from .groceries import strPopen
+from threading import Thread
 
-def checkth(sleeptime:int):
-  exename ='Wuthering Waves.exe'
-  exename2 = 'AntiCheatExpert Service'
-  while True:
-    sleep(sleeptime)
-    if len(strPopen(f'tasklist | find "{exename}"'))>10: #有無運行
-        if int(strPopen(f'sc query "{exename2}" | find /c "STATE"'))>0:  #服務狀態
-          strPopen(f'sc stop "{exename2}"')  #關閉服務
+
+class checkth():
+
+  def __init__(self,sleeptime:int):
+    self.sleeptime=sleeptime
+    self.breakcl=False
+    self.startth=Thread(target=self.th)
+
+  def th(self):
+    exename ='Wuthering Waves.exe'
+    exename2 = 'AntiCheatExpert Service'
+    while True:
+      sleep(self.sleeptime)
+      if len(strPopen(f'tasklist | find "{exename}"'))>10: #有無運行
+          if int(strPopen(f'sc query "{exename2}" | find /c "STATE"'))>0:  #服務狀態
+            strPopen(f'sc stop "{exename2}"')  #關閉服務
+
+      if self.breakcl:
+        break
+
+  def start(self):
+    if not self.startth.is_alive():
+      self.startth=Thread(target=self.th)
+      self.startth.start()
+
+  def stop(self):
+    self.breakcl=True
